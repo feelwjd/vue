5fdd3a2526e996d8b7c06415f42e476bbca2421c
feelwjd <feelwjd@gmail.com>
2021-12-07 10:51:24 +0900

    sd

--- src/views/exercise1.vue
+++ src/views/exercise1.vue
@@ -1,9 +1,49 @@
 <template>
-  <div>
-    <h1>exercise1</h1>
-    <p> 홈페이지 입니다.<br>
-    201735951
-        민정필 </p>
+<div>
+<h1>{{animal}} 안녕 ???</h1>
+<h2>{{animal}}는 {{food1}}를 먹습니다</h2>
+<h2>{{animal}}는 {{food2}}를 좋아합니다. </h2>
+<input type="text" v-model="animal" /><hr/>
 
-  </div>
+<h2> {{alertMessage}} </h2>
+<div v-html="alertMessage"> </div>
+<a :href="naverUrl">naver</a>
+<hr/>
+
+<input type="text" v-model="food3" /><hr/>
+<h2 v-bind:class ="{red : food3 ==='apple', blue : food3 ==='banana'}">
+                   {{animal}}는 {{food3}}를 좋아합니다. 
+</h2>
+</div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      animal : "원숭이",
+      food1 : "apple",
+      food2 : "banana",
+      food3 : "banana",           
+      alertMessage : "<h2>경고!!!</h2>",
+      naverUrl : "https://www.naver.com",
+    };
+  }
+}
+</script>
+
+<style scoped>
+input {
+  font-size : 20px;
+}
+orange {
+    color : orange ;
+}
+.red {
+    color : red ;
+}
+.blue {
+    color : blue ;
+}
+</style>
